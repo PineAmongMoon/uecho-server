032939ac7b48fa04b2ca65e95d7300b10d0c253a
Ju Yuanmao <juyuanmao@icloud.com>
2019-03-18 11:10:55 +0800

    update

--- uecho_server.c
+++ uecho_server.c
@@ -5,28 +5,21 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1200
+#define BUFFER_SIZE 1056
 #define PORT 60000
 
-int main()
+void mainloop(int server_socket);
+void create_sock(int * sock);
+void create_addr(struct sockaddr_in * sock_addr, uint32_t host, uint16_t port);
+
+int main(int argc, char const *argv[])
 {
     int server_socket;
-    uint8_t massage[BUFFER_SIZE];
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len;
-    size_t str_len;
+    struct sockaddr_in server_addr;
 
-    server_socket = socket(PF_INET, SOCK_DGRAM, 0);
-    if (server_socket == -1)
-    {
-        perror("UDP scoket creation error!");
-        exit(1);
-    }
+    create_sock(&server_socket);
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(PORT);
+    create_addr(&server_addr, INADDR_ANY, PORT);
     
     if (bind(server_socket, (const struct sockaddr*)&server_addr, sizeof(server_addr)) != 0)
     {
@@ -34,8 +27,36 @@ int main()
         exit(2);
     }
 
-    memset(massage, 0, sizeof(massage));
+    mainloop(server_socket);
 
+    close(server_socket);
+    return 0;
+}
+
+void create_sock(int * sock)
+{
+    *sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (*sock == -1)
+    {
+        perror("UDP scoket creation error!");
+        exit(1);
+    }
+}
+
+void create_addr(struct sockaddr_in * sock_addr, uint32_t host, uint16_t port)
+{
+    memset(sock_addr, 0, sizeof(struct sockaddr_in));
+    sock_addr->sin_family = AF_INET;
+    sock_addr->sin_addr.s_addr = htonl(host);
+    sock_addr->sin_port = htons(port);
+}
+
+void mainloop(int server_socket)
+{
+    socklen_t client_addr_len;
+    size_t str_len;
+    uint8_t massage[BUFFER_SIZE];
+    struct sockaddr_in client_addr;
     for(;;)
     {
         client_addr_len = sizeof(client_addr);
@@ -43,7 +64,5 @@ int main()
             (struct sockaddr*)&client_addr, &client_addr_len);
         sendto(server_socket, massage, str_len, 0, 
             (struct sockaddr*)&client_addr, client_addr_len);
-    } 
-    close(server_socket);
-    return 0;
+    }
 }
